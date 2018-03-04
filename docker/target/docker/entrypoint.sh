d6d51d6cdaa77c99618924f5249c15fa989c673c
Renato Mendes <renato.mendes.1123@gmail.com>
2018-03-04 11:27:31 -0500

    Commit

--- docker/target/docker/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ $USE_CLOUD == 'true' ]
-then
-echo "Using Spring Cloud Config Profile:" ${USE_PROFILES} Url: ${USE_URL} Label: ${USE_LABEL}
-JAVA_OPTS=-Dspring.cloud.config.uri=${USE_URI} -Dspring.cloud.config.label=${USE_LABEL} -Dspring.application.profiles=${USE_PROFILES}
-fi
-
-exec java $JAVA_OPTS -jar $ENV_JAR_NAME "$@"
